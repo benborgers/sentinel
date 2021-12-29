fff247d9108f0a28ca2300b535d8cb2ba91fd131
Ben Borgers <borgersbenjamin@gmail.com>
2021-12-28 21:38:11 -0500

    Hide errors

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,16 @@
-import { MetaFunction, HeadersFunction, LoaderFunction, redirect } from "remix";
-import { useLoaderData, json } from "remix";
+import React, { useState, useEffect } from "react";
+import {
+  MetaFunction,
+  HeadersFunction,
+  LoaderFunction,
+  redirect,
+  ActionFunction,
+} from "remix";
+import { useLoaderData, json, Form, useTransition } from "remix";
 import prisma from "~/lib/prisma.server";
 import { Prisma } from "@prisma/client";
+import { XIcon } from "@heroicons/react/solid";
+import { Popover } from "@headlessui/react";
 
 export const meta: MetaFunction = () => ({
   title: "Sentinel",
@@ -39,6 +48,7 @@ export const loader: LoaderFunction = async ({ request }) => {
   const PER_PAGE = 25;
 
   const errors = await prisma.error.findMany({
+    where: { hidden: false },
     orderBy: { createdAt: "desc" },
     take: PER_PAGE,
     skip: page * PER_PAGE,
@@ -47,8 +57,45 @@ export const loader: LoaderFunction = async ({ request }) => {
   return json<LoaderData>({ authorized: true, errors });
 };
 
+export const action: ActionFunction = async ({ request }) => {
+  if (!isAuthorized(request)) throw new Response("", { status: 401 });
+
+  const formData = await request.formData();
+  const action = formData.get("_action");
+  const search = new URLSearchParams(new URL(request.url).search);
+
+  switch (action) {
+    case "hide_error": {
+      await prisma.error.update({
+        where: { id: search.get("id") || undefined },
+        data: { hidden: true },
+      });
+
+      return redirect("/");
+    }
+  }
+};
+
 export default function Index() {
-  const data: LoaderData = useLoaderData();
+  const loaderData = useLoaderData();
+  const [data, setData] = useState<LoaderData>(loaderData);
+  useEffect(() => setData(loaderData), [loaderData]);
+
+  const transition = useTransition();
+
+  useEffect(() => {
+    if (
+      transition.state === "submitting" &&
+      transition.submission.formData.get("_action") === "hide_error"
+    ) {
+      const clone = { ...data };
+      const search = new URLSearchParams(transition.submission.action);
+      clone.errors = clone.errors.filter(
+        (error) => error.id !== search.get("id")
+      );
+      setData(clone);
+    }
+  }, [transition.state]);
 
   if (!data.authorized) {
     return <p>Unauthorized</p>;
@@ -62,11 +109,31 @@ export default function Index() {
             key={error.id}
             className="bg-white shadow border border-gray-200 p-4 rounded-xl"
           >
-            <div className="flex gap-x-3 items-start">
-              <p className="mt-0.5 text-red-600 bg-red-100 px-2 py-0.5 rounded-lg max-w-max font-medium text-sm">
-                {error.appName}
-              </p>
-              <p className="text-xl font-bold text-gray-900">{error.message}</p>
+            <div className="flex justify-between items-start">
+              <div className="flex gap-x-3 items-start">
+                <p className="mt-0.5 text-red-600 bg-red-100 px-2 py-0.5 rounded-lg max-w-max font-medium text-sm">
+                  {error.appName}
+                </p>
+                <p className="text-xl font-bold text-gray-900">
+                  {error.message}
+                </p>
+              </div>
+
+              <Popover className="relative">
+                <Popover.Button className="mt-1.5">
+                  <XIcon className="h-4 w-4 text-gray-400" />
+                  <p className="sr-only">Delete Error</p>
+                </Popover.Button>
+
+                <Popover.Panel className="absolute top-7 right-0 z-50 w-max shadow-xl rounded-lg bg-red-500 text-white">
+                  <Form method="post" action={`/?index&id=${error.id}`}>
+                    <input name="_action" value="hide_error" type="hidden" />
+                    <button className="font-semibold px-2 py-1">
+                      Yes, delete this error
+                    </button>
+                  </Form>
+                </Popover.Panel>
+              </Popover>
             </div>
 
             <pre className="overflow-scroll mt-4 bg-gray-900 text-white text-sm rounded-lg p-3">
